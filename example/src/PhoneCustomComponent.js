90244fa0c93971d15865f34f037c73895680657d
José Trinidad Espinoza González <trinoeg8@gmail.com>
2020-04-10 11:43:52 -0500

    Add PhoneCustomComponent example
    - This example is about displaying the result of a custom component
    with a select and an text input and making it editable.
    - Shows how customizable this library is by allowing the use of any
    component.

---
+++ example/src/PhoneCustomComponent.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import { useForm } from 'react-hook-form';
+
+const PhoneCustomComponent = (props) => {
+  const currentPhone = props.value.phone;
+  const currentOption = props.value.type;
+  const {
+    handleSubmit,
+    register,
+    errors,
+  } = useForm();
+
+  const submit = (values) => {
+    props.submit(values);
+  };
+
+  const getOptions = () => {
+    return props.value.options.map(option => (
+      <option value={option.value} key={'option-' + option.value}>
+        {option.label}
+      </option>
+    ));
+  };
+
+  return (
+    <div className="custom-component">
+      <form onSubmit={handleSubmit(submit)}>
+        <div className="form-group row">
+          <div className="col-sm-3 p-0">
+            <select
+              defaultValue={currentOption}
+              className="form-control"
+              name="type"
+              ref={
+                register({required: "Required"})
+              }
+            >
+              {getOptions()}
+            </select>
+          </div>
+          <div className="col-sm-6 p-0">
+            <input
+              type="text"
+              name="phone"
+              id="phone"
+              autoFocus
+              autoComplete="off"
+              className="form-control"
+              defaultValue={currentPhone}
+              ref={
+                register({required: "Required"})
+              }
+            />
+            { errors.phone &&
+              <div className="">
+                {errors.phone.message}
+              </div>
+            }
+          </div>
+          <div>
+            {props.buttons}
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default PhoneCustomComponent;
